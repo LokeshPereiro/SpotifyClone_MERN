ff4ff8d42b0d2b15f9cc20efb492b13aefa49a42
Lokesh Pereiro <lpdraft001@gmail.com>
2023-11-29 14:22:22 +0100

    remove duplicated files

--- src/store/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { songSlice } from "./slices/song";
-export const store = configureStore({
-  reducer: {
-    mainSong: songSlice.reducer,
-  },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({ serializableCheck: false }),
-});
